c587c745645538e832b1aba0dd713f750d9f62e9
fernandomora <carlos.mora@appgate.com>
2022-06-23 20:49:37 -0500

    define test

--- bowling-game/src/test/java/com/appgate/dtp/tdd/dojo/GameTest.java
+++ bowling-game/src/test/java/com/appgate/dtp/tdd/dojo/GameTest.java
@@ -1,10 +1,9 @@
 package com.appgate.dtp.tdd.dojo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.fail;
 
 @DisplayName("Bowling Game")
 class GameTest {
@@ -15,13 +14,24 @@ class GameTest {
 
     }
 
+    /**
+     * Initial Condition
+     */
     @Test
     @Order(1)
     @DisplayName("Initial condition")
-    void shouldReturnZeroWhenGameStarted(){
+    void shouldReturnZeroWhenGameStarted() {
 //        Given
+        Game game = new Game();
 //        When
+        int score = game.score();
 //        Then
-        fail();
+        Assertions.assertEquals(0, score);
     }
+    /**
+     * Invalid inputs
+     */
+    /**
+     *
+     */
 }
