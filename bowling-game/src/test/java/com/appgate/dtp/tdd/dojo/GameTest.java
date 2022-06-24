e137b31217192945d54fc28bf5bf176cc76ec795
fernandomora <carlos.mora@appgate.com>
2022-06-23 21:09:05 -0500

    test 4 in red

--- bowling-game/src/test/java/com/appgate/dtp/tdd/dojo/GameTest.java
+++ bowling-game/src/test/java/com/appgate/dtp/tdd/dojo/GameTest.java
@@ -7,7 +7,7 @@ class GameTest {
     Game game;
 
     @BeforeEach
-    void init(){
+    void init() {
         game = new Game();
     }
 
@@ -31,22 +31,32 @@ class GameTest {
     @Test
     @Order(2)
     @DisplayName("should return error when the number of pins knocked down is more than limit")
-    void shouldReturnErrorWhenNumberOfPinsKnockedDownIsMoreThanLimit(){
+    void shouldReturnErrorWhenNumberOfPinsKnockedDownIsMoreThanLimit() {
 //        When
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
             game.roll(11);
         });
     }
+
     @Test
     @Order(3)
     @DisplayName("should return error when the number of pins knocked down is under limit")
-    void shouldReturnErrorWhenNumberOfPinsKnockedDownIsUnderLimit(){
+    void shouldReturnErrorWhenNumberOfPinsKnockedDownIsUnderLimit() {
 //        When
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
             game.roll(-1);
         });
     }
+
     /**
-     *
+     * functionality
      */
+    @Test
+    @Order(4)
+    @DisplayName("should return score when roll a valid pins knocked down")
+    void shouldReturnScoreWhenRollAndPinsKnockedDownIsValid() {
+//        When
+        game.roll(5);
+        Assertions.assertEquals(5, game.score());
+    }
 }
